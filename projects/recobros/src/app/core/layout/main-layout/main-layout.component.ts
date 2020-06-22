0e0785704c717810f7f017b8a4daf5b064061e4a
Himad Mouhtar <himadmouhtar@gmail.com>
2020-06-22 18:44:23 +0200

    Style changes

--- projects/recobros/src/app/core/layout/main-layout/main-layout.component.ts
+++ projects/recobros/src/app/core/layout/main-layout/main-layout.component.ts
@@ -1,9 +1,11 @@
-import { Component, OnInit } from "@angular/core";
+import { Component, OnInit, Renderer2 } from "@angular/core";
 import { MatSnackBar } from "@angular/material/snack-bar";
 import { AlertService } from "../../services/alert.service";
 import { AuthenticationService } from "../../services/authentication.service";
 import { SidenavService } from "../../services/sidenav.service";
 import { UserService } from "../../services/user.service";
+import { Router, NavigationStart } from "@angular/router";
+import { filter } from "rxjs/operators";
 
 @Component({
   selector: "alvea-main-layout",
@@ -11,6 +13,7 @@ import { UserService } from "../../services/user.service";
   styleUrls: ["./main-layout.component.scss"],
 })
 export class MainLayoutComponent implements OnInit {
+  private previousUrl: string;
   public isUserLoggedIn: boolean;
   public sidenavLinks: any;
   public currentUser;
@@ -21,9 +24,23 @@ export class MainLayoutComponent implements OnInit {
     private authenticationService: AuthenticationService,
     private _snackBar: MatSnackBar,
     private sidenavService: SidenavService,
-    private userService: UserService
+    private userService: UserService,
+    private router: Router,
+    private renderer: Renderer2
   ) {}
   ngOnInit(): void {
+    this.router.events
+
+      .pipe(filter((event) => event instanceof NavigationStart))
+      .subscribe((event) => {
+        event["url"] = event["url"].split("?")[0];
+        console.log(event);
+
+        this.renderer.addClass(document.body, event["url"]);
+        this.renderer.removeClass(document.body, this.previousUrl);
+        this.previousUrl = event["url"];
+      });
+
     this.authenticationService.isUserLoggedInO.subscribe((res) => {
       this.isUserLoggedIn = res;
       if (this.isUserLoggedIn) {
