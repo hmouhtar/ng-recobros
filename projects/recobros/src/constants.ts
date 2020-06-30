c22f4fd1e0f6f8e0a4551715e89135ef47daa49c
Himad Mouhtar <himadmouhtar@gmail.com>
2020-06-30 13:21:11 +0200

    changed constants

--- projects/recobros/src/constants.ts
+++ projects/recobros/src/constants.ts
@@ -1,4 +1,3 @@
 export class Config {
-  public static apiURL: string =
-    'http://eurekaauth.eu-west-3.elasticbeanstalk.com';
+  public static apiURL = 'https://eurekaauth.eu-west-3.elasticbeanstalk.com';
 }
